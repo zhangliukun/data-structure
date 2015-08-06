f1161095f12c19dbe0f74c2b2328d175b4af8a1d
zhangliukun <1433317488@qq.com>
2015-08-06 23:59:52 +0800

    fix the bug of the binaryInsertSort

--- src/sort/BinaryInsertSort.java
+++ src/sort/BinaryInsertSort.java
@@ -2,38 +2,40 @@ package sort;
 
 public class BinaryInsertSort {
 
-	public static void binaryInsertSort(int[] num){
-	
-		
+	public static void binaryInsertSort(int[] num) {
+
 		for (int j = 1; j < num.length; j++) {
-			for(int i = 0;i<j;i++){
-				
-				int temp = num[j];
-				int insertPosition = binarySearchInsertPosition(num, num[i], 0, j-1);
-				for(int k =j;k>insertPosition;k--){
-					num[k] = num[k-1];
-				}
-				num[i] = temp;
+			int temp = num[j];
+
+			int insertPosition = binarySearchInsertPosition(num, temp, 0, j - 1);
+			for (int k = j; k > insertPosition; k--) {
+				num[k] = num[k - 1];
+			}
+			num[insertPosition] = temp;
+		}
+	}
+
+	public static int binarySearchInsertPosition(int[] num, int target,
+			int startIndex, int endIndex) {
+
+		if (startIndex >= endIndex) {
+			if (num[startIndex] < target)
+				return startIndex + 1;
+			else{
+				return startIndex;
 			}
 		}
-	}
-	
-	public static int  binarySearchInsertPosition(int[] num,int target,int startIndex,int endIndex){
-		
-		if (startIndex == endIndex) {
-			return startIndex;
+
+		int middle = (startIndex + endIndex) / 2;
+		if (target > num[middle]) {
+			return binarySearchInsertPosition(num, target, middle + 1, endIndex);
+		} else if (target < num[middle]) {
+			return binarySearchInsertPosition(num, target, startIndex,
+					middle - 1);
+		} else {
+			return middle + 1;
 		}
-		
-		int middle = (startIndex + endIndex)/2;
-		if (target>num[middle]) {
-			return binarySearchInsertPosition(num, target, middle+1,endIndex);
-		}else if(target <num[middle]){
-			return binarySearchInsertPosition(num, target, startIndex, middle-1);
-		}else {
-			return middle+1;
-		}
-		
+
 	}
-	
 	
 }
