a07664764a279eb5d70257ab1c36ae12434fae43
zhangliukun <1433317488@qq.com>
2015-08-11 00:35:42 +0800

    shell sort

--- src/data/TestSortData.java
+++ src/data/TestSortData.java
@@ -2,7 +2,6 @@ package data;
 
 import java.util.Random;
 
-
 import sort.BinaryInsertSort;
 import sort.HeapSort;
 import sort.InsertSort;
@@ -23,17 +22,17 @@ public class TestSortData {
 		 */
 
 		// 测试数据
-		int[] num = new int[3000];
+		int[] num = new int[100000];
 		Random random = new Random();
 		for (int i = 0; i < num.length; i++) {
-			num[i] = random.nextInt(100);
+			num[i] = random.nextInt(100000);
 		}
 
 		// int[] num = new int[]{74,79,26,3,33};
 
 		System.out.print("原数组为:");
-		
-		ShowResult.showArrays(num);
+
+		//ShowResult.showArrays(num);
 		System.out.println();
 
 		int[] copyNum = new int[num.length];
@@ -46,79 +45,88 @@ public class TestSortData {
 		// 快速排序
 
 		System.arraycopy(num, 0, copyNum, 0, num.length);
-		long startTime=System.currentTimeMillis();
+		long startTime = System.currentTimeMillis();
 		QuickSortV1.quickSort(copyNum, 0, copyNum.length - 1);
 		long endTime = System.currentTimeMillis();
 		System.out.print("快速排序的结果为:");
-		System.out.println((endTime - startTime)+"ms");
-		ShowResult.showArrays(copyNum);
+		System.out.println((endTime - startTime) + "ms");
+		// ShowResult.showArrays(copyNum);
 
 		// 冒泡排序
 		System.arraycopy(num, 0, copyNum, 0, num.length);
-		startTime=System.currentTimeMillis();
-		PopSort.popSort(copyNum);
+		startTime = System.currentTimeMillis();
+		//PopSort.popSort(copyNum);
 		endTime = System.currentTimeMillis();
 		System.out.print("冒泡排序的结果为:");
-		ShowResult.showArrays(copyNum);
-		System.out.println((endTime - startTime)+"ms");
+		// ShowResult.showArrays(copyNum);
+		System.out.println((endTime - startTime) + "ms");
 
 		// 选择排序
 		System.arraycopy(num, 0, copyNum, 0, num.length);
-		startTime=System.currentTimeMillis();
-		SelectSort.selectSort(copyNum);
+		startTime = System.currentTimeMillis();
+		//SelectSort.selectSort(copyNum);
 		endTime = System.currentTimeMillis();
 		System.out.print("选择排序的结果为:");
-		ShowResult.showArrays(copyNum);
-		System.out.println((endTime - startTime)+"ms");
+		// ShowResult.showArrays(copyNum);
+		System.out.println((endTime - startTime) + "ms");
 
 		// 插入排序
 		System.arraycopy(num, 0, copyNum, 0, num.length);
-		startTime=System.currentTimeMillis();
-		InsertSort.insertSrot(copyNum);
+		startTime = System.currentTimeMillis();
+		//InsertSort.insertSrot(copyNum);
 		endTime = System.currentTimeMillis();
 		System.out.print("插入排序的结果为:");
-		ShowResult.showArrays(copyNum);
-		System.out.println((endTime - startTime)+"ms");
+		// ShowResult.showArrays(copyNum);
+		System.out.println((endTime - startTime) + "ms");
 
 		// 二分插入排序
 		System.arraycopy(num, 0, copyNum, 0, num.length);
-		startTime=System.currentTimeMillis();
-		BinaryInsertSort.binaryInsertSort(copyNum);
+		startTime = System.currentTimeMillis();
+		//BinaryInsertSort.binaryInsertSort(copyNum);
 		endTime = System.currentTimeMillis();
 		System.out.print("二分插入排序的结果为:");
-		ShowResult.showArrays(copyNum);
-		System.out.println((endTime - startTime)+"ms");
+		// ShowResult.showArrays(copyNum);
+		System.out.println((endTime - startTime) + "ms");
 
 		// 归并排序
 		System.arraycopy(num, 0, copyNum, 0, num.length);
-		startTime=System.currentTimeMillis();
-		int[] temp = new int[100000];
-		startTime=System.currentTimeMillis();
-		MergeSort.mergeSortDivide(copyNum, 0, copyNum.length-1, temp);
+		startTime = System.currentTimeMillis();
+		int[] temp = new int[500000];
+		startTime = System.currentTimeMillis();
+		MergeSort.mergeSortDivide(copyNum, 0, copyNum.length - 1, temp);
 		endTime = System.currentTimeMillis();
 		System.out.print("归并排序的结果为:");
-		//ShowResult.showArrays(copyNum);
-		System.out.println((endTime - startTime)+"ms");
-		
-		//堆排序
+		// ShowResult.showArrays(copyNum);
+		System.out.println((endTime - startTime) + "ms");
+
+		// 堆排序
 		System.arraycopy(num, 0, copyNum, 0, num.length);
 		System.out.print("堆排序的结果为:");
-		startTime=System.currentTimeMillis();
-		HeapSort.buildHeap(copyNum, (copyNum.length -1)/2, copyNum.length);
-		ShowResult.showArrays(copyNum);
-		HeapSort.deleteMinHead(copyNum,(num.length-2)/2, num.length);
+		startTime = System.currentTimeMillis();
+		HeapSort.buildHeap(copyNum, (copyNum.length - 1) / 2, copyNum.length);
+		// ShowResult.showArrays(copyNum);
+		HeapSort.deleteMinHead(copyNum, (num.length - 2) / 2, num.length);
 		endTime = System.currentTimeMillis();
-		System.out.println((endTime - startTime)+"ms");
-		
-		//希尔排序
+		System.out.println((endTime - startTime) + "ms");
+
+		// 希尔排序
 		System.arraycopy(num, 0, copyNum, 0, num.length);
-		startTime=System.currentTimeMillis();
-		ShellSort.shellSort(copyNum);
+		startTime = System.currentTimeMillis();
+		//ShellSort.shellSort(copyNum);
 		endTime = System.currentTimeMillis();
 		System.out.print("希尔排序的结果为:");
-		ShowResult.showArrays(copyNum);
-		System.out.println((endTime - startTime)+"ms");
-		
+		//ShowResult.showArrays(copyNum);
+		System.out.println((endTime - startTime) + "ms");
+
+		// 希尔排序
+		System.arraycopy(num, 0, copyNum, 0, num.length);
+		startTime = System.currentTimeMillis();
+		ShellSort.shellSrot2(copyNum);
+		endTime = System.currentTimeMillis();
+		System.out.print("希尔排序2的结果为:");
+		//ShowResult.showArrays(copyNum);
+		System.out.println((endTime - startTime) + "ms");
+
 	}
 
 }
--- src/sort/ShellSort.java
+++ src/sort/ShellSort.java
@@ -1,41 +1,44 @@
 package sort;
 
 public class ShellSort {
-	
-	public static void shellSort(int[] num){
-		
-		int b = num.length/2;
-		
-		while(b!=0){
-			
-			for(int i= 0;i<num.length;i++){
-				
-				for(int j= i;j+b<num.length;j = j+b){
-					
-					
-					for(int k=j+b;k<num.length;k=k+b){
-						int temp;
-						if (num[k] < num[j]) {
-							temp = num[j];
-							for(int m =j;m+b<k;m = m+b){
-								
+
+	public static void shellSort(int[] num) {
+
+		int b = num.length / 3;
+
+		while (b != 0) {
+
+			for (int i = 0; i < b; i++) {
+
+				for (int j = i + b; j < num.length; j = j + b) {
+
+					for (int k = i; k < j; k = k + b) {
+						if (num[j] < num[k]) {
+							int temp = num[j];
+							for (int m = j; m - b >= k; m = m - b) {
+								num[m] = num[m - b];
 							}
-							
+							num[k] = temp;
 						}
 					}
-					
-//					int temp;
-//					if (num[j+b]<num[j]) {
-//						temp = num[j];
-//						num[j] = num[j+b];
-//						num[j+b] = temp;
-//					}
 				}
-				
 			}
-			b = b/2;
-			
+			b = b / 3;
 		}
-		
 	}
+
+	public static void shellSrot2(int[] a) {  
+        int i, j, temp, increment;  
+        // increment增量缩短，当增量为1时，即把整个数组进行插入排序  
+        for (increment = a.length / 3; increment > 0; increment /= 3) {  
+            for (i = increment; i < a.length; i++) {  
+                temp = a[i];  
+                for (j = i - increment; j >= 0 && temp < a[j]; j -= increment) {  
+                    a[j + increment] = a[j];  
+                }  
+                a[j + increment] = temp;  
+            }  
+  
+        }  
+    }  
 }
